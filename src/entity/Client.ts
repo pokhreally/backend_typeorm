7c6fbf3848bdca2a0e8a6818ff8ce835357d43d7
Sulav Pokhrel <sulav.pokhrel25@gmail.com>
2023-11-28 20:51:47 +0545

    initial commit

---
+++ src/entity/Client.ts
@@ -0,0 +1,32 @@
+import { Entity, BaseEntity, Column } from "typeorm";
+
+@Entity("client")
+export class Client extends BaseEntity {
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column({
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    unique: true,
+    length: 10,
+  })
+  card_number: string;
+
+  @Column({
+    type: "numeric",
+  })
+  balance: number;
+
+  @Column({
+    default: true,
+    name: "active",
+  })
+  is_active: boolean;
+}
